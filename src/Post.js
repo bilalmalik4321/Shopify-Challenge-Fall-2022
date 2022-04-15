d76d6614a12cc55bd50426fc651ff75068e8ecd0
Bilal Malik <bilalmalik4321@gmail.com>
2022-04-15 17:43:11 -0400

    added liking icon

--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Favorite from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 import './Post.css'
 
 function Post({imageUrl, title, date, description}) {
@@ -7,10 +11,18 @@ function Post({imageUrl, title, date, description}) {
     <div className="post">
 
         <img className="post__image" src={imageUrl} alt=""/>
-
-        <h4 className="post__text"><strong>{title}</strong> - {date}</h4>
-
-        <p className="post__description">{description}</p>
+        
+        <div className="post__footer">
+            <div className="post__footer_lead">
+                <FormControlLabel
+                    control={<Checkbox icon={<FavoriteBorder />} 
+                            checkedIcon={<Favorite />}
+                    name="checkedH" />}
+                />
+                <h4 className="post__text"><strong>{title}</strong> - {date}</h4>
+            </div>
+            <p className="post__description">{description}</p>
+        </div>
 
     </div>
   )
